d4fc6abc0550efe062e448e5f7f01975305ef4da
Amidwar <amidwar05@gmail.com>
2025-06-13 23:27:17 -0500

    EJERCICIO 1 - AVANCE 2 : AGREGANDO UN BOCETO DEL PROGRAMA GENERAL, POSIBLES ERORRES DE GENERADOR DE NUMEROS

--- PROBLEMA_1.cpp
+++ PROBLEMA_1.cpp
@@ -4,4 +4,69 @@ generará su jugada de forma aleatoria. El programa determinará el ganador de cad
 reglas del juego. El juego continuará hasta que el usuario o la computadora gane 3 rondas. Muestra el 
 resultado final indicando el ganador del juego. */
 
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+	int JUGADA,CPU, VIDAS_DEL_USUARIO = 3;
+	srand(time(0)); // inicializamos el generador de numeros aleatorios
+	do{
+		cout<<"Digite el numero de su jugada."<<endl;
+		cout<<"1. PIEDRA."<<endl;
+		cout<<"2. PAPEL."<<endl;
+		cout<<"3. TIJERA."<<endl;
+		cin>>JUGADA;
+		
+		
+		CPU = rand() % 3 + 1;
+		if(JUGADA == CPU){
+			cout<<"EL CPU ELIGIO "<<CPU<<" TU ELEGISTE "<<JUGADA<<endl;
+			cout<<"EMPATE"<<endl;
+		}
+		
+		if(JUGADA == 1 && CPU == 3){
+			cout<<"EL CPU ELIGIO "<<CPU<<" TU ELEGISTE "<<JUGADA<<endl;
+			cout<<"FELICIDADES, GANASTE ESTA RONDA."<<endl;
+		}
+		
+		if(JUGADA == 1 && CPU == 2){
+			cout<<"EL CPU ELIGIO "<<CPU<<" TU ELEGISTE "<<JUGADA<<endl;
+			cout<<"PERDISTE ESTA RONDA."<<endl;
+			VIDAS_DEL_USUARIO -= 1;
+		}
+		
+		if(JUGADA == 3 && CPU == 2){
+			cout<<"EL CPU ELIGIO "<<CPU<<" TU ELEGISTE "<<JUGADA<<endl;
+			cout<<"FELICIDADES, GANASTE ESTA RONDA."<<endl;
+		}
+		if(JUGADA == 3 && CPU == 1){
+			cout<<"EL CPU ELIGIO "<<CPU<<" TU ELEGISTE "<<JUGADA<<endl;
+			cout<<"PERDISTE ESTA RONDA."<<endl;
+			VIDAS_DEL_USUARIO -= 1;
+		}
+		if(JUGADA == 2 && CPU == 1){
+			cout<<"EL CPU ELIGIO "<<CPU<<" TU ELEGISTE "<<JUGADA<<endl;
+			cout<<"FELICIDADES, GANASTE ESTA RONDA."<<endl;
+		}
+		if(JUGADA == 2 && CPU == 3){
+			cout<<"EL CPU ELIGIO "<<CPU<<" TU ELEGISTE "<<JUGADA<<endl;
+			cout<<"PERDISTE ESTA RONDA."<<endl;
+			VIDAS_DEL_USUARIO -= 1;
+		}
+		
+		
+	}while(VIDAS_DEL_USUARIO != 0);
+	
+	
+	
+	return 0;
+} 
+	
+	
+	
+	
+	
+
 
