5607bdff7b8188b00f052e99fbc113348a3c5782
Amidwar <amidwar05@gmail.com>
2025-06-13 22:33:42 -0500

    Avance 1: Agregando el ejerciocio 6

---
+++ PROBLEMA_1.cpp
@@ -0,0 +1,7 @@
+/* 6. Juego de Piedra, Papel y Tijeras: Desarrolla un programa que simule el juego de piedra, papel y 
+tijeras contra la computadora. El usuario elegirá su jugada (piedra, papel o tijeras) y la computadora 
+generará su jugada de forma aleatoria. El programa determinará el ganador de cada ronda según las 
+reglas del juego. El juego continuará hasta que el usuario o la computadora gane 3 rondas. Muestra el 
+resultado final indicando el ganador del juego. */
+
+
